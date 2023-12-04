08105fb4f194f4439851e81c49a0d7319f4744a1
wu-wilson <wilson.cui.wu@gmail.com>
2023-12-04 00:14:58 -0500

    increased max character limit

--- src/pages/workshop/Workshop.tsx
+++ src/pages/workshop/Workshop.tsx
@@ -7,7 +7,7 @@ import styles from "./workshop.module.scss";
 
 const instructions =
   "\nNeed help editing a small passage of text? Start by pasting the text you'd like help with below.\n\nOnce you are done, choose the Daemon you'd like to assist you. Then, we'll ask the Daemon where and how your text could be improved.";
-const maxCharacters = 500;
+const maxCharacters = 1000;
 
 const Workshop = ({
   text,
